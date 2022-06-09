3b14614a50cca958ef1e76c6d0d5c9684681559e
Cameron Tauxe <ctauxe@neptuneinc.org>
2022-06-09 12:43:09 -0600

    Refactor flake

--- flake.nix
+++ flake.nix
@@ -25,74 +25,18 @@
 
   outputs = { self, nixpkgs, flake-compat, flake-utils, mach-nix }:
     flake-utils.lib.eachDefaultSystem (system:
+      with builtins;
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        myPkgs = import ./pkgs.nix { inherit pkgs; };
       in
-      with builtins;
-      with pkgs;
-      with pkgs.python310Packages;
-      let
-
-        # Git repo for hic-straw
-        straw-repo = fetchGit {
-          url="https://github.com/aidenlab/straw";
-          rev="bc91874571ecd3a50c80cc42582b14147e500d68"; # Latest commit as of June 7, 2022
-        };
-
-        # hic-straw python package
-        hic-straw = buildPythonPackage {
-            pname = "hic-straw";
-            version = "1.3.1";
-            src = "${straw-repo}/pybind11_python";
-            propagatedBuildInputs = [ pybind11 curl ];
-        };
-
-        # Dependencies for hic2structure
-        deps = [ hic-straw numpy scipy ];
-
-        # hic2structure python package
-        hic2structure = buildPythonPackage {
-            pname = "hic2structure";
-            version = "0.2";
-            src = ./.;
-            propagatedBuildInputs = deps;
-            doCheck = false;
-          };
-
-        # LAMMPS executable
-        lammps = stdenv.mkDerivation {
-          # Based off June 2022 release
-          name = "lammps-220602";
-          src = fetchGit {
-            url="https://github.com/lammps/lammps";
-            rev="ceb9466172398e9a20cb510528b4b17f719c7cf2";
-          };
-          buildInputs = [ cmake ];
-          configurePhase = ''
-            mkdir build
-            cd build
-            cmake ../cmake
-            cmake -C ../cmake/presets/basic.cmake ../cmake
-          '';
-          buildPhase = ''
-            cmake --build . --parallel ''${NIX_BUILD_CORES:-1}
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            cp -t $out/bin lmp
-          '';
-
-        };
-
-      in rec {
+      rec {
         # Export packages
-        packages = { inherit hic2structure hic-straw lammps; };
+        packages = { inherit (myPkgs) hic2structure hic-straw lammps; };
         defaultPackage = packages.hic2structure;
 
         # Development environment
-        devShell = mkShell {
-          buildInputs = [python310 lammps] ++ deps;
-        };
+        devShell = myPkgs.devShell;
       }
     );
 }
---
+++ pkgs.nix
@@ -0,0 +1,67 @@
+{ pkgs }:
+
+let
+  # Git repo for hic-straw
+  straw-repo = fetchGit {
+    url="https://github.com/aidenlab/straw";
+    rev="bc91874571ecd3a50c80cc42582b14147e500d68"; # Latest commit as of June 7, 2022
+  };
+
+  # hic-straw python package
+  hic-straw = { python3, curl }: with python3.pkgs; buildPythonPackage {
+    pname = "hic-straw";
+    version = "1.3.1";
+    src = "${straw-repo}/pybind11_python";
+    propagatedBuildInputs = [ pybind11 curl ];
+  };
+
+  # Dependencies for hic2structure
+  pydeps = { python3 }: with python3.pkgs; [ 
+    (pkgs.callPackage hic-straw {inherit python3;})
+    numpy scipy 
+  ];
+
+  # hic2structure python package
+  hic2structure = { python3 }: python3.pkgs.buildPythonPackage {
+    pname = "hic2structure";
+    version = "0.2";
+    src = ./.;
+    propagatedBuildInputs = pydeps {inherit python3;};
+    doCheck = false;
+  };
+
+  # Development shell
+  devShell = { mkShell, python3 }: with python3.pkgs; mkShell {
+    buildInputs = [ python3 (pkgs.callPackage lammps {}) ] ++ (pydeps {inherit python3;});
+  };
+
+  # LAMMPS executable
+  lammps = { stdenv, cmake }: stdenv.mkDerivation {
+    # Based off June 2022 release
+    name = "lammps-220602";
+    src = fetchGit {
+      url="https://github.com/lammps/lammps";
+      rev="ceb9466172398e9a20cb510528b4b17f719c7cf2";
+    };
+    buildInputs = [ cmake ];
+    configurePhase = ''
+      mkdir build
+      cd build
+      cmake ../cmake
+      cmake -C ../cmake/presets/basic.cmake ../cmake
+    '';
+    buildPhase = ''
+      cmake --build . --parallel ''${NIX_BUILD_CORES:-1}
+    '';
+    installPhase = ''
+      mkdir -p $out/bin
+      cp -t $out/bin lmp
+    '';
+  };
+in
+{
+  hic-straw = pkgs.callPackage hic-straw {};
+  hic2structure = pkgs.callPackage hic2structure {};
+  lammps = pkgs.callPackage lammps {};
+  devShell = pkgs.callPackage devShell {};
+}
